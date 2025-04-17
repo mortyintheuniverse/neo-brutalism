eaf4b555dde3d472c92fe9174fdb050eeb5b4f3f
Morty Wu <68367033+mortyintheuniverse@users.noreply.github.com>
2025-04-17 13:54:36 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Set the response header to return JSON
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+
+  // Create a sample JSON object
+  const response = {
+    message: 'Hello from Node server!',
+    success: true
+  };
+
+  // Send JSON response
+  res.end(JSON.stringify(response));
+});
+
+// Start server on port 80
+server.listen(80, () => {
+  console.log('Server is running on port 80');
+});
